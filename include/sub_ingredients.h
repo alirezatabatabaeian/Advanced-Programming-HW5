02d00579bb3b19e586203b0248a3973b9e4667e5
Alireza <alireza2541@aut.ac.ir>
2022-05-23 20:07:19 +0430

    7: Challange Completed

--- include/sub_ingredients.h
+++ include/sub_ingredients.h
@@ -2,94 +2,117 @@
 #define SUB_INGREDIENTS_H
 #include "ingredient.h"
 
-// ######################################################### //
-class Cinnamon : public Ingredient {
-public:
-    Cinnamon(size_t units)
-        : Ingredient { 5, units }
-    {
-        this->name = "Cinnamon";
-    }
+// ******************************************** Challange ********************************************* //
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Chocolate : public Ingredient {
-public:
-    Chocolate(size_t units)
-        : Ingredient { 5, units }
-    {
-        this->name = "Chocolate";
-    }
+#define DEFCLASS(Ingredient_Name, Price_Per_Unit)       \
+                                                        \
+    class Ingredient_Name : public Ingredient {         \
+    public:                                             \
+        Ingredient_Name(size_t units)                   \
+            : Ingredient { Price_Per_Unit, units }      \
+        {                                               \
+            name = #Ingredient_Name;                    \
+        }                                               \
+        virtual std::string get_name() { return name; } \
+    };
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Sugar : public Ingredient {
-public:
-    Sugar(size_t units)
-        : Ingredient { 1, units }
-    {
-        this->name = "Sugar";
-    }
+DEFCLASS(Cinnamon, 5);
+DEFCLASS(Chocolate, 5);
+DEFCLASS(Sugar, 1);
+DEFCLASS(Cookie, 10);
+DEFCLASS(Espresso, 15);
+DEFCLASS(Milk, 10);
+DEFCLASS(MilkFoam, 5);
+DEFCLASS(Water, 1);
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Cookie : public Ingredient {
-public:
-    Cookie(size_t units)
-        : Ingredient { 10, units }
-    {
-        this->name = "Cookie";
-    }
+// // ######################################################### //
+// class Cinnamon : public Ingredient {
+// public:
+//     Cinnamon(size_t units)
+//         : Ingredient { 5, units }
+//     {
+//         this->name = "Cinnamon";
+//     }
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Espresso : public Ingredient {
-public:
-    Espresso(size_t units)
-        : Ingredient { 15, units }
-    {
-        this->name = "Espresso";
-    }
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Chocolate : public Ingredient {
+// public:
+//     Chocolate(size_t units)
+//         : Ingredient { 5, units }
+//     {
+//         this->name = "Chocolate";
+//     }
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Milk : public Ingredient {
-public:
-    Milk(size_t units)
-        : Ingredient { 10, units }
-    {
-        this->name = "Milk";
-    }
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Sugar : public Ingredient {
+// public:
+//     Sugar(size_t units)
+//         : Ingredient { 1, units }
+//     {
+//         this->name = "Sugar";
+//     }
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class MilkFoam : public Ingredient {
-public:
-    MilkFoam(size_t units)
-        : Ingredient { 5, units }
-    {
-        this->name = "MilkFoam";
-    }
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Cookie : public Ingredient {
+// public:
+//     Cookie(size_t units)
+//         : Ingredient { 10, units }
+//     {
+//         this->name = "Cookie";
+//     }
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
-class Water : public Ingredient {
-public:
-    Water(size_t units)
-        : Ingredient { 1, units }
-    {
-        this->name = "Water";
-    }
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Espresso : public Ingredient {
+// public:
+//     Espresso(size_t units)
+//         : Ingredient { 15, units }
+//     {
+//         this->name = "Espresso";
+//     }
 
-    virtual std::string get_name() { return this->name; }
-};
-// ######################################################### //
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Milk : public Ingredient {
+// public:
+//     Milk(size_t units)
+//         : Ingredient { 10, units }
+//     {
+//         this->name = "Milk";
+//     }
+
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class MilkFoam : public Ingredient {
+// public:
+//     MilkFoam(size_t units)
+//         : Ingredient { 5, units }
+//     {
+//         this->name = "MilkFoam";
+//     }
+
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
+// class Water : public Ingredient {
+// public:
+//     Water(size_t units)
+//         : Ingredient { 1, units }
+//     {
+//         this->name = "Water";
+//     }
+
+//     virtual std::string get_name() { return this->name; }
+// };
+// // ######################################################### //
 
 #endif // SUB_INGREDIENTS_H
