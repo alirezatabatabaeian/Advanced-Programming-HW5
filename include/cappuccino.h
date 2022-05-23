5241d12ddfda9ba98696ee7a0f9c3f2bd4f393a8
Alireza <alireza2541@aut.ac.ir>
2022-05-23 16:22:27 +0430

    3: Cappuccino Class Completed

--- include/cappuccino.h
+++ include/cappuccino.h
@@ -1,24 +1,24 @@
-// #ifndef CAPPUCCINO
-// #define CAPPUCCINO
-// #include "espresso_based.h"
-// #include "ingredient.h"
-// #include "sub_ingredients.h"
+#ifndef CAPPUCCINO
+#define CAPPUCCINO
+#include "espresso_based.h"
+#include "ingredient.h"
+#include "sub_ingredients.h"
 
-// class Cappuccino : public EspressoBased {
-// public:
-//     Cappuccino();
-//     Cappuccino(const Cappuccino& cap);
-//     // ~Cappuccino();
-//     void operator=(const Cappuccino& cap);
+class Cappuccino : public EspressoBased {
+public:
+    Cappuccino();
+    Cappuccino(const Cappuccino& cap);
+    // ~Cappuccino();
+    void operator=(const Cappuccino& cap);
 
-//     virtual std::string get_name() { return this->name; }
-//     virtual double price();
+    virtual std::string get_name() { return this->name; }
+    virtual double price();
 
-//     void add_side_item(Ingredient* side) { side_items.push_back(side); }
-//     std::vector<Ingredient*>& get_side_items() { return side_items; }
+    void add_side_item(Ingredient* side) { side_items.push_back(side); }
+    std::vector<Ingredient*>& get_side_items() { return side_items; }
 
-// private:
-//     std::vector<Ingredient*> side_items;
-// };
+private:
+    std::vector<Ingredient*> side_items;
+};
 
-// #endif // CAPPUCCINO
+#endif // CAPPUCCINO
