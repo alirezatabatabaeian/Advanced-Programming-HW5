0a842f5020dad63b493f3e4d72085dcaa52b2771
Alireza <alireza2541@aut.ac.ir>
2022-05-23 16:22:03 +0430

    2: EspressoBased Class Completed

--- src/cappuccino.cpp
+++ src/cappuccino.cpp
@@ -1,47 +1,47 @@
-#include "cappuccino.h"
+// #include "cappuccino.h"
 
-// #################################### //
-Cappuccino::Cappuccino()
-    : side_items {}
-{
-    this->name = "Cappuccino";
-    this->ingredients.push_back(new Espresso { 2 });
-    this->ingredients.push_back(new Milk { 2 });
-    this->ingredients.push_back(new MilkFoam { 1 });
-}
-// #################################### //
-Cappuccino::Cappuccino(const Cappuccino& cap)
-    : side_items { cap.side_items }
-{
-    this->name = cap.name;
-    this->ingredients = cap.ingredients;
-}
-// #################################### //
-void Cappuccino::operator=(const Cappuccino& cap)
-{
-    side_items = cap.side_items;
-    this->name = cap.name;
-    this->ingredients = cap.ingredients;
-}
-// #################################### //
-// Cappuccino::~Cappuccino()
+// // #################################### //
+// Cappuccino::Cappuccino()
+//     : side_items {}
 // {
-
+//     this->name = "Cappuccino";
+//     this->ingredients.push_back(new Espresso { 2 });
+//     this->ingredients.push_back(new Milk { 2 });
+//     this->ingredients.push_back(new MilkFoam { 1 });
 // }
-// #################################### //
-double Cappuccino::price()
-{
-    double _price {};
-    for (size_t i {}; i < ingredients.size(); i++) {
-        _price += ingredients[i]->price();
-    }
-    if (!side_items.empty()) {
-        for (size_t i {}; i < side_items.size(); i++) {
-            _price += side_items[i]->price();
-        }
-    }
-    return _price;
-}
-// #################################### //
-// #################################### //
-// #################################### //
+// // #################################### //
+// Cappuccino::Cappuccino(const Cappuccino& cap)
+//     : side_items { cap.side_items }
+// {
+//     this->name = cap.name;
+//     this->ingredients = cap.ingredients;
+// }
+// // #################################### //
+// void Cappuccino::operator=(const Cappuccino& cap)
+// {
+//     side_items = cap.side_items;
+//     this->name = cap.name;
+//     this->ingredients = cap.ingredients;
+// }
+// // #################################### //
+// // Cappuccino::~Cappuccino()
+// // {
+
+// // }
+// // #################################### //
+// double Cappuccino::price()
+// {
+//     double _price {};
+//     for (size_t i {}; i < ingredients.size(); i++) {
+//         _price += ingredients[i]->price();
+//     }
+//     if (!side_items.empty()) {
+//         for (size_t i {}; i < side_items.size(); i++) {
+//             _price += side_items[i]->price();
+//         }
+//     }
+//     return _price;
+// }
+// // #################################### //
+// // #################################### //
+// // #################################### //
